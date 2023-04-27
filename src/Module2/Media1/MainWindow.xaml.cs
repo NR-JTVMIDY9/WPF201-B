696e95cedab42c48a1c89a6e9600f550fdc56dfd
Phillip Madden <symbolicon@live.com>
2023-04-27 17:11:14 +0800

    Added Module2 content

---
+++ src/Module2/Media1/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Media1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            media1.Play();
+        }
+
+        private void BtnPause_Click(object sender, RoutedEventArgs e)
+        {
+            if (media1.CanPause)
+                media1.Pause();
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            media1.Stop();
+        }
+
+        private void BtnMute_Click(object sender, RoutedEventArgs e)
+        {
+            media1.IsMuted = !media1.IsMuted;
+        }
+
+        private void Media1_MediaOpened(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Media1_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+
+        }
+
+        private void Media1_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        //private void HslVolume_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+        //    try
+        //    {
+        //        media1.Volume = hslVolume.Value;
+        //    }   catch {  }
+        //}
+    }
+}
---
+++ src/Module2/Windows1/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Windows1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private Window1 window1;
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (window1 != null)
+            {
+                window1.Focus();
+                return;
+            }
+
+            window1 = new Window1();
+            window1.Closed += (ws, we) => window1 = null;
+            window1.Owner = this;
+            window1.Show();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window2();
+            window.Owner = this;
+            window.Show();
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window3();
+            window.Owner = this;
+            window.Show();
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window4();
+            window.Owner = this;
+            window.Show();
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window5();
+            window.Owner = this;
+            window.Show();
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window6();
+            window.Owner = this;
+            window.Show();
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+            //Application app = App.Current;
+            //Window mainWindow = app.MainWindow;
+            //WindowCollection windows = app.Windows;
+            //foreach (Window window in windows)
+            //    if (window != mainWindow) window.Close();
+            //    foreach (Window window in App.Current.Windows)
+            //        if (window != this) window.Close();
+            foreach (Window window in OwnedWindows)
+                window.Close();
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window7();
+            window.Owner = this;
+            bool? result = window.ShowDialog();
+            if (result == true)
+            {
+                MessageBox.Show("Window6 return true");
+            }
+        }
+    }
+}
